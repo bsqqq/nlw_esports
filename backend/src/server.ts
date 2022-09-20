3bd95fa66451bc6b7263ad144cf0dc177834c554
Vinicius Moura <Vinicius.Moura@hogarthww.com>
2022-09-20 18:18:22 -0300

    iniital commit

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/ads', (req, res) => {
+    return res.status(200).json([
+        {id: 1, nome: 'ad 1'},
+        {id: 2, nome: 'ad 2'},
+        {id: 3, nome: 'ad 3'},
+        {id: 4, nome: 'ad 4'},
+    ])
+})
+
+app.listen(3333, () => console.log('running at 3333'))
